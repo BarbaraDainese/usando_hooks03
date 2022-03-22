98df076402b39bf4aa59057d0f11dc1ab533a563
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-22 11:20:46 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useMemo} from 'react';
+
 
 function App() {
+
+  const[tarefas, setTarefas] = useState([]);
+  const[input, setInput] = useState('');
+
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('tarefas');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(()=>{
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  function handleAdd(){
+
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+  const totalTarefas = useMemo(()=> tarefas.length, [tarefas])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <br/>
+      <strong>Voce tem {totalTarefas} tarefas!</strong><br/>
+      <input typr="text" value={input} onChange={e=> setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
